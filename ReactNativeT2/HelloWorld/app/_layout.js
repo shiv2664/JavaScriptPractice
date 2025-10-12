9e221623fd8511e6ca68192ec1a72ba5206ca3a2
shiv2664 <shivamsehrawat38@gmail.com>
2025-10-12 13:57:52 +0530

    Login Form Text Input

--- ReactNativeT2/HelloWorld/app/_layout.js
+++ ReactNativeT2/HelloWorld/app/_layout.js
@@ -1,12 +1,13 @@
-import { View, Text, StyleSheet, ScrollView,Platform,FlatList } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Platform, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BasicComponent from '../components/ui/basicComponents';
 import Box from '../components/ui/box';
 import WindowSizeComponent from '../components/ui/window_dimensions';
 import { StatusBar } from 'expo-status-bar';
 import PokemonCardParent from '../components/PokemonCard/PokemonCardParent';
-import  ListingExample from '../components/ListComponent/ListingExample';
+import ListingExample from '../components/ListComponent/ListingExample';
 import pokemonList from '../assets/data.json'
+import LoginForm from '../components/LoginForm/LoginForm';
 
 export default function RootLayout() {
   return (
@@ -15,17 +16,9 @@ export default function RootLayout() {
         backgroundColor="plum"     // matches container background
         barStyle="dark-content"    // use "light-content" for dark bg
         translucent={false} />
-         <FlatList
-            data={pokemonList}
-            renderItem={({ item }) => {
-                return (
-                    <View style={styles.card}>
-                        <Text>{item.id}</Text>
-                        <Text>{item.name}</Text>
-                        <Text>{item.type}</Text>
-                    </View>
-                );
-            }}></FlatList>
+        <LoginForm>
+
+        </LoginForm>
 
     </SafeAreaView>
   );
@@ -36,9 +29,9 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: "plum",
-    flexDirection: "column", // vertical layout
-    alignItems: "left",     // horizontal center
-    justifyContent: "center", // vertical center
+    flexDirection: "row", // vertical layout
+    // alignItems:"center",     // horizontal center
+    // justifyContent: "center", // vertical center
     columnGap: 10
 
   },
@@ -60,27 +53,41 @@ const styles = StyleSheet.create({
     backgroundColor: "red"
   },
   card: {
-        backgroundColor: "white",
-        borderRadius: 16,
-        borderWidth: 2,
-        padding: 16,
-        margin: 16,
-        width: "90%",
-        ...Platform.select({
-            ios: {
-                shadowOffset: { width: 2, height: 2 },
-                shadowColor: "#333",
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
-            },
-            android: {
-                elevation: 5,
-            },
-        }),
-    }
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    width: "90%",
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  }
 })
 
 
+
+{/* <FlatList
+        data={pokemonList}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.card}>
+              <Text>{item.id}</Text>
+              <Text>{item.name}</Text>
+              <Text>{item.type}</Text>
+            </View>
+          );
+        }}
+        ListEmptyComponent={<Text>No Items Found</Text>}>
+      </FlatList> */}
 
 // <SafeAreaView style={styles.container}>
 //    <StatusBar
